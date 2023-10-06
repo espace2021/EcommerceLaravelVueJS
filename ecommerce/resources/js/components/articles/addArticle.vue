3da047241d3f907536c7d1f8cb9540854d247cae
sh <sandrahammamitlili@gmail.com>
2023-10-06 11:08:18 +0200

    filepond-commit

--- ecommerce/resources/js/components/articles/addArticle.vue
+++ ecommerce/resources/js/components/articles/addArticle.vue
@@ -1,139 +1,143 @@
-<template lang="">
+<template>
+  <div>
     <form @submit.prevent="ajouterproduit">
      
-       <div class="form-group">
-       <input type="text" class="form-control" placeholder="reference" v-model="article.reference">
-     </div>
      <div class="form-group">
-       <input type="text" class="form-control" placeholder="designation" v-model="article.designation">
+     <input type="text" class="form-control" placeholder="reference" v-model="article.reference">
+   </div>
+   <div class="form-group">
+     <input type="text" class="form-control" placeholder="designation" v-model="article.designation">
+   </div>
+   <div class="form-group">
+     <input type="text" class="form-control" placeholder="marque" v-model="article.marque">
+   </div>
+  <div class="form-group">
+     <input type="number" class="form-control" placeholder="prix" v-model="article.prix">
+   </div>
+  <div class="form-group">
+     <input type="number" class="form-control" placeholder="qtestock" v-model="article.qtestock">
+   </div>
+  <div class="form-group">
+     Sous Catégories<select class="form-control" v-model="article.scategorieID">
+        <option v-for="sc in Scategories" :key="sc.id" :value=sc.id>{{sc.nomscategorie}}</option>
+     </select>  
      </div>
-     <div class="form-group">
-       <input type="text" class="form-control" placeholder="marque" v-model="article.marque">
-     </div>
-    <div class="form-group">
-       <input type="number" class="form-control" placeholder="prix" v-model="article.prix">
-     </div>
-    <div class="form-group">
-       <input type="number" class="form-control" placeholder="qtestock" v-model="article.qtestock">
-     </div>
-    <div class="form-group">
-       Sous Catégories<select class="form-control" v-model="article.scategorieID">
-          <option v-for="sc in Scategories" :key="sc.id" :value=sc.id>{{sc.nomscategorie}}</option>
-       </select>  
-     </div>
-     <div class="form-group">
-      <div>
-    <file-pond
-      ref="filepond"
-      name="file"
-      :files="files"
-      label-idle="Glissez et déposez votre fichier ou <span class='filepond--label-action'>cliquez pour parcourir</span>"
-      allow-multiple="false"
-      :server="serverOptions"
-      @processfile="handleFileUpload"
-    ></file-pond>
-    <img :src="uploadedImage" v-if="uploadedImage" alt="Uploaded Image" />
+
+      <!-- FilePond pour le téléchargement d'images -->
+      <div class="form-group">
+        <div>
+          
+          <file-pond
+            name="test"
+            ref="pond"
+            class-name="my-pond"
+            label-idle="Drop files here..."
+            allow-multiple="false"
+            accepted-file-types="image/jpeg, image/png"
+            v-bind:files="myFiles"
+            v-on:init="handleFilePondInit"
+            :server="serverOptions()"
+          />
+
+        </div>
+      </div>
+
+      <button type="submit" class="btn btn-block btn-primary">Ajouter Produit</button>
+    </form>
   </div>
-    </div>
-    <button type="submit" class="btn btn-block btn-primary">Ajouter Produit</button>
-   </form>
-   
-   </template>
-   
-    
-   <script setup>
-import { ref, onMounted, computed } from 'vue';
+</template>
+
+<script setup>
+/*
+En utilisant le préfixe : (liaison dynamique), vous indiquez à Vue.js 
+d'évaluer la valeur de la propriété serverOptions comme une expression JavaScript plutôt que de considérer 
+"serverOptions" comme une URL. Cela devrait résoudre l'erreur "405 (Method Not Allowed)" 
+Avec () après serverOptions on appelle réellement la fonction serverOptions pour obtenir les options du serveur 
+au lieu de simplement passer la référence de la fonction. 
+*/
+import { ref, onMounted } from 'vue';
 
 import vueFilePond from 'vue-filepond';
 import 'filepond/dist/filepond.min.css';
-
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
 
-let FilePond= vueFilePond(FilePondPluginImagePreview);
+// Create FilePond component
+const FilePond = vueFilePond(FilePondPluginImagePreview);
 
-Vue.use(FilePond);
-
-let files= ref([]);
-let uploadedImage= ref("");
-
-import { useCloudinary } from '../config/useCloudinary.js';
-
-const { url, transformations } = useCloudinary(props.src)
+const myFiles = ref([]);
 
 import axios from "../config/axios.js";
-   
 import { useRouter } from 'vue-router';
-const router = useRouter() 
+
+const router = useRouter();
 
 const Scategories = ref([]);
 
- onMounted(() => {
-    getscategories();
-            }
-);
+const getScategories = () => {
+  axios.get('/api/scategories')
+    .then(res => {
+      Scategories.value = res.data;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
 
-const getscategories=()=>{
-          
-    axios.get('/api/scategories').then(res => {
-        Scategories.value = res.data;
-            }).catch(error => {
-                console.log(error)
-            });
+onMounted(() => {
+  getScategories();
+});
 
-         }
+const article = {
+  reference: "",
+  designation: "",
+  marque: "",
+  prix: "",
+  qtestock: "",
+  imageart: "",
+  scategorieID: "",
+};
 
-let  article= {
-               reference: "",
-               designation: "",
-               marque: "",
-               prix: "",
-               qtestock: "",
-               imageart: "",
-               scategorie:"",
-       }
-      
-       
 const  ajouterproduit=()=>{
-   
-          axios.post("/api/articles",article)
-               .then(() => {
-                     router.push('/articles')})
-               .catch(error => {
-                  console.error("There was an error!", error);})
-                  }
-             
-const  handleFileUpload=()=> {
-      uploadedImage.value = files.value[0].serverId;
-    } 
-    
-    computed(() => {
-     serverOptions=()=> {
-      return {
-        process: (fieldName, file, metadata, load, error, progress, abort) => {
-         url(
-            file,
-            {
-              folder: "uploads",
-            },
-            (error, result) => {
-              if (error) {
-                console.error(error);
-                error("Upload failed");
-              } else {
-                load(result.secure_url);
-              }
-            },
-            {
-              resource_type: "auto",
-            }
-          );
-        }
-      }
- 
-  }   
-})
+   console.log(article)
+   axios.post("/api/articles",article)
+        .then(() => {
+              router.push('/articles')})
+        .catch(error => {
+           console.error("There was an error!", error);})
+           }
 
-   </script>
- 
-   
+
+
+const handleFilePondInit = () => {
+  console.log('FilePond has initialized');
+}
+
+const serverOptions = () => { console.log('server pond');
+  return {
+    process: (fieldName, file, metadata, load, error, progress, abort) => {
+      const data = new FormData();
+      
+      data.append('file', file);
+      data.append('upload_preset', 'Ecommerce_cloudinary');
+      data.append('cloud_name', 'iset-sfax');
+      data.append('public_id', file.name);
+
+      axios.post('https://api.cloudinary.com/v1_1/iset-sfax/image/upload', data)
+        .then((response) => response.data)
+        .then((data) => {
+          console.log(data);
+          article.imageart = data.url;
+          load(data);
+        })
+        .catch((error) => {
+          console.error('Error uploading file:', error);
+          error('Upload failed');
+          abort();
+        });
+    },
+  };
+};
+
+
+</script>
