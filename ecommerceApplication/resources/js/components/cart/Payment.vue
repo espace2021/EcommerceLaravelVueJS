72bd2b403d1740e287cfe406c3070f77edc73e09
sh <sandrahammamitlili@gmail.com>
2023-11-30 15:44:37 +0100

    multiStore-commit

--- ecommerceApplication/resources/js/components/cart/Payment.vue
+++ ecommerceApplication/resources/js/components/cart/Payment.vue
@@ -66,7 +66,7 @@ const handleSubmit = async () => {
       // Le paiement a réussi
       alert(response.data.message);
       //Vider le cart
-      store.commit('.Articlestore/clearCart')
+      store.commit('Articlestore/clearCart')
       //Redirection
       router.push('/shopping')
     } else {
